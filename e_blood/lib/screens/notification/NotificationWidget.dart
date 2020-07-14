5b56a76ce1a5ce36165a7bb1492af81656ca2d3b
FaizanKamal7 <57044214+FaizanKamal7@users.noreply.github.com>
2020-07-14 19:05:00 +0500

    E-Blood
    1.4

--- e_blood/lib/screens/notification/NotificationWidget.dart
+++ e_blood/lib/screens/notification/NotificationWidget.dart
@@ -6,158 +6,179 @@ class NotificationWidget extends StatelessWidget {
     final deviceSize = MediaQuery.of(context).size;
     return Container(
       child: Container(
-        
-        margin: EdgeInsets.only(top: deviceSize.height*0.01,),
+        margin: EdgeInsets.only(
+          top: deviceSize.height * 0.01,
+        ),
         width: deviceSize.width,
         height: deviceSize.height * 0.23,
-         
-        
-
         decoration: BoxDecoration(
-             color: Colors.white,
-          borderRadius: BorderRadius.circular(20.0,),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(
+            20.0,
+          ),
           boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.5),
-        spreadRadius: 5,
-        blurRadius: 7,
-        offset: Offset(0, 3), // changes position of shadow
-      ),
-    ],
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
         ),
-        
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-           Container(
-             //margin: EdgeInsets.only(left: deviceSize.width *0.02),
-             padding: EdgeInsets.only(left: deviceSize.width *0.02,top: deviceSize.height*0.004),
-                    height: deviceSize.height*0.03,
-                    width: deviceSize.width,
-                    color: Colors.grey[200],
-                    child: Text(
-                      'Blood Request From  *Name of patient *',
-                    ),
-
-                  ),
             Container(
-              margin: EdgeInsets.only(left:  deviceSize.width*0.02, top:  deviceSize.height*0.01, right:  deviceSize.width*0.01,),
-                  child:RichText(
-                    text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    children: <TextSpan>[
-                   
-                   TextSpan(
-                     text: "Patient Name  :  ",
-                    style: TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                   ),
-                    TextSpan(
-                     text: "Zeeshan Bashir   ",
-                    style: TextStyle(
-                        fontSize: 15, ),
-                   ),
-                        ],
-                    ),
+              //margin: EdgeInsets.only(left: deviceSize.width *0.02),
+              padding: EdgeInsets.only(
+                  left: deviceSize.width * 0.02,
+                  top: deviceSize.height * 0.004),
+              height: deviceSize.height * 0.03,
+              width: deviceSize.width,
+              color: Colors.grey[200],
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Blood Request :',
                   ),
-                  
-            
-              
-            ),
-             Container(
-              margin: EdgeInsets.only(left: 10, top:  deviceSize.height*0.01),
-              child:  RichText(
-                    text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    children: <TextSpan>[
-                   
-                   TextSpan(
-                     text: "Blood Group :  ",
-                    style: TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                   ),
-                    TextSpan(
-                     text: "O+",
-                    style: TextStyle(
-                        fontSize: 15, ),
-                   ),
-                        ],
-                    ),
-                  ),
+                  Spacer(),
+                  Container(
+                    margin: EdgeInsets.only(right: deviceSize.width * 0.03),
+                    child: Text('Time'),
+                  )
+                ],
+              ),
             ),
             Container(
-              margin: EdgeInsets.only(right:  deviceSize.width*0.01, left: deviceSize.width*0.02,top:  deviceSize.height*0.01),
-              child:  RichText(
-                    text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    children: <TextSpan>[
-                   
-                   TextSpan(
-                     text: "Location :  ",
-                    style: TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                   ),
+              margin: EdgeInsets.only(
+                left: deviceSize.width * 0.02,
+                top: deviceSize.height * 0.01,
+                right: deviceSize.width * 0.01,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
                     TextSpan(
-                     text: "Abbottabad   ",
-                    style: TextStyle(
-                        fontSize: 15, ),
-                   ),
-                        ],
+                      text: "Patient Name  :  ",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                     ),
-                  ),
+                    TextSpan(
+                      text: "Zeeshan Bashir   ",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 10, top:  deviceSize.height*0.01),
-              child:  RichText(
-                    text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
-                    children: <TextSpan>[
-                   
-                   TextSpan(
-                     text: "Description :  ",
-                    style: TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold),
-                   ),
+              margin: EdgeInsets.only(left: 10, top: deviceSize.height * 0.01),
+              child: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
                     TextSpan(
-                     text: "Urgent Blood needed for thalesemia Ptient in ayub medical complex   ",
-                    style: TextStyle(
-                        fontSize: 15, ),
-                   ),
-                        ],
+                      text: "Blood Group :  ",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                     ),
-                  ),
+                    TextSpan(
+                      text: "O+",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Container(
-              margin: EdgeInsets.only(right:  deviceSize.width*0.01,top:deviceSize.width*0.0 ),
+              margin: EdgeInsets.only(
+                  right: deviceSize.width * 0.01,
+                  left: deviceSize.width * 0.02,
+                  top: deviceSize.height * 0.01),
+              child: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "Location :  ",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: "Abbottabad   ",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10, top: deviceSize.height * 0.01),
+              child: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "Description :  ",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text:
+                          "Urgent Blood needed for thalesemia Ptient in ayub medical complex   ",
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  right: deviceSize.width * 0.01, top: deviceSize.width * 0.0),
               child: Row(
                 children: <Widget>[
                   Spacer(),
                   Container(
-                    height: deviceSize.height *0.04,
-                    width: deviceSize.height *0.04,
+                    height: deviceSize.height * 0.04,
+                    width: deviceSize.height * 0.04,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0,),
-                      border: Border.all(
-                        
-                        color: Colors.green,
-                      )
+                        borderRadius: BorderRadius.circular(
+                          50.0,
+                        ),
+                        border: Border.all(
+                          color: Colors.green,
+                        )),
+                    child: Icon(
+                      Icons.phone,
+                      color: Colors.green,
                     ),
-                    child: Icon(Icons.phone,
-                    color: Colors.green,),),
-                  SizedBox(width:  deviceSize.width*0.07),
+                  ),
+                  SizedBox(width: deviceSize.width * 0.07),
                   Container(
-                    height: deviceSize.height *0.04,
-                    width: deviceSize.height *0.04,
+                    height: deviceSize.height * 0.04,
+                    width: deviceSize.height * 0.04,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50.0,),
-                      border: Border.all(
-                        
-                        color: Colors.green,
-                      )
-                    ),child: Icon(Icons.message,
-                    color: Colors.green,
-                    ),),
-                  SizedBox(width:  deviceSize.width*0.04),
+                        borderRadius: BorderRadius.circular(
+                          50.0,
+                        ),
+                        border: Border.all(
+                          color: Colors.green,
+                        )),
+                    child: Icon(
+                      Icons.message,
+                      color: Colors.green,
+                    ),
+                  ),
+                  SizedBox(width: deviceSize.width * 0.04),
                 ],
               ),
             )
